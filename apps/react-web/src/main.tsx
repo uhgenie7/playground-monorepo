bfe36b42184c7c94e0082d3c467c90089bf1128b
HyejinUm <uhj1993@naver.com>
2023-01-08 00:05:37 +0900

    react-router-dom v6.6

--- apps/react-web/src/main.tsx
+++ apps/react-web/src/main.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root';
+import ErrorPage from './404';
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    loader: rootLoader,
+    action: rootAction,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: 'contacts/:contactId',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: 'contacts/:contactId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: 'contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
