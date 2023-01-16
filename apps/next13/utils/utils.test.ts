f8e0d9689d1e54c35b129e08b69e825ce0bcea46
HyejinUm <uhj1993@naver.com>
2023-01-16 21:52:31 +0900

    🧪 test code

--- apps/next13/utils/utils.test.ts
+++ apps/next13/utils/utils.test.ts
@@ -1,8 +1,8 @@
 describe('계산 테스트', () => {
   const a = 1,
-    b = 2;
+    b = 3;
 
-  test('a + b는 3이다.', () => {
-    expect(a + b).toEqual(3);
+  test('a + b는 4이다.', () => {
+    expect(a + b).toEqual(4);
   });
 });
